b1a609b66b3e63fd04cddade068cfeb80a625f0c
Viviane-Silva <viviane.leite.silva@gmail.com>
2022-04-19 11:37:52 -0300

    scripts

---
+++ script.js
@@ -0,0 +1,41 @@
+function desenhaQuadrado(x,y,color){
+    let tela = document.querySelector('canvas');
+    let pincel = tela.getContext('2d');
+
+    pincel.fillStyle = color;
+    pincel.fillStroke = 'black';
+    pincel.fillRect (x,y,50,50);
+    pincel.strokeRect (x,y,50,50);
+
+    
+}
+// usando laço while
+// let x =0;
+// while(x < 600){
+//     desenhaQuadrado(x,0,'yellow');
+//     x = x + 50;
+// }
+
+// usando laço for
+for (let x=0; x<600; x=x + 50){
+    desenhaQuadrado(x,0,'blue')
+}
+
+desenhaQuadrado(0,0 , 'red');
+// desenhaQuadrado(50,0 , 'blue');
+// desenhaQuadrado(0,50 , 'green');
+// desenhaQuadrado(0,100);
+// desenhaQuadrado(50,100, 'yellow');
+// -----------------------------------------------------------
+
+let tela = document.querySelector('canvas');
+let caneta = tela.getContext('2d');
+
+caneta.fillStyle = 'black';
+caneta.fillRect(0,0,600,400);
+
+function exibeAlerta(){
+    alert('Clicou');
+}
+
+tela.onclick = exibeAlerta;
